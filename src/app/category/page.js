c9371e4516c4cde0ca65284ae54bcf7ad94605c9
Evan <evan@vers.kr>
2023-12-21 10:05:47 +0900

    [style] 메인페이지 이미지변경 카테고리 스타일 변경

--- src/app/category/page.js
+++ src/app/category/page.js
@@ -4,14 +4,14 @@
 import "./category.scss";
 import { useRouter } from "next/navigation";
 import Header from "@/components/Header/Header";
-import allBg from '../../../public/allBg.svg';
-import redWineBg from '../../../public/redWineBg.svg';
-import whiteWineBg from '../../../public/whiteWineBg.svg';
-import roseWineBG from '../../../public/roseWineBG.svg';
-import sparklingWineBg from '../../../public/sparklingWineBg.svg';
-import dessertWineBg from '../../../public/dessertWineBg.svg';
-import nonAlcoholBg from '../../../public/nonAlcoholBg.svg';
-import snackBg from '../../../public/snackBg.svg';
+import category_all from '../../../public/category_all.svg';
+import category_redWine from '../../../public/category_redWine.svg';
+import category_whiteWine from '../../../public/category_whiteWine.svg';
+import category_roseWine from '../../../public/category_roseWine.svg';
+import category_sparklingWine from '../../../public/category_sparklingWine.svg';
+import category_dessertWine from '../../../public/category_dessertWine.svg';
+import category_nonAlcohol from '../../../public/category_nonAlcohol.svg';
+import category_snack from '../../../public/category_snack.svg';
 import Image from "next/image";
 
 export default function Category() {
@@ -19,14 +19,14 @@ export default function Category() {
     const router = useRouter();
 
     const testBox = [
-        { "img": allBg, "name": "전체보기", id: 1 },
-        { "img": redWineBg, "name": "레드 와인", id: 2 },
-        { "img": whiteWineBg, "name": "화이트 와인", id: 3 },
-        { "img": roseWineBG, "name": "로제 와인", id: 4 },
-        { "img": sparklingWineBg, "name": "스파클링 와인", id: 5 },
-        { "img": dessertWineBg, "name": "디저트 와인", id: 6 },
-        { "img": nonAlcoholBg, "name": "논알코올", id: 7 },
-        { "img": snackBg, "name": "안주", id: 8 },
+        { "img": category_all, "name": "전체보기", id: 1 },
+        { "img": category_redWine, "name": "레드 와인", id: 2 },
+        { "img": category_whiteWine, "name": "화이트 와인", id: 3 },
+        { "img": category_roseWine, "name": "로제 와인", id: 4 },
+        { "img": category_sparklingWine, "name": "스파클링 와인", id: 5 },
+        { "img": category_dessertWine, "name": "디저트 와인", id: 6 },
+        { "img": category_nonAlcohol, "name": "논알코올", id: 7 },
+        { "img": category_snack, "name": "안주", id: 8 },
     ]
 
     return (
@@ -37,13 +37,12 @@ export default function Category() {
                     {testBox?.map((item, idx) => {
                         return (
                             <div
-                                style={{ background: `${item.name === "전체보기" ? "#FF6135" : "black"}` }}
                                 key={idx}
                                 className="category-itemBox"
                                 onClick={() => { router.push(`/products/${item.id}`) }}
                             >
-                                <p>{item.name}</p>
                                 <Image src={item.img} alt={item.name} />
+                                <p>{item.name}</p>
                             </div>
                         )
                     })}
