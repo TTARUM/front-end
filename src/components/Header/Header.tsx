bb2f4f3c7a603ab6c47d2bea5d7173dbc2d2758f
박준석 <parkjunseok@bagjunseog-ui-MacBookAir.local>
2024-01-10 23:50:15 +0900

    [style] 공백제거

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,10 +1,8 @@
 'use client';
 
 import './Header.scss';
-
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-
 import Logo from '../../../public/Logo.svg';
 import searchIcon from '../../../public/search.svg';
 import heartIcon from '../../../public/heart.svg';
