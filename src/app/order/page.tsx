c6c58a8317fd4f115db234098f9b342404eb489b
박준석 <parkjunseok@bagjunseog-ui-MacBookAir.local>
2024-02-19 23:34:59 +0900

    [style] TTAR-22 주문/결제 페이지 추가

---
+++ src/app/order/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+import Header from "@/components/Header/Header"
+
+const Order = () => {
+  return (
+    <div>
+        <Header title="주문결제" type="subMenu"/>
+    </div>
+  )
+}
+
+export default Order
