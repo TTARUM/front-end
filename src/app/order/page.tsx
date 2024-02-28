a0f37beff9d9a7eb72d04c36f7cc54e7b458ce70
evan <evan@vers.kr>
2024-02-28 15:06:37 +0900

    [feat] url에 전달된 query를 파싱해서 Json으로 변환

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -1,13 +1,44 @@
-'use client'
+'use client';
 
-import Header from "@/components/Header/Header"
+import { useEffect, useState } from 'react';
+import Header from '@/components/Header/Header';
+
+type queryData = {
+  id: number;
+  img: {
+    src: string;
+    height: number;
+    width: number;
+    blurHeight: number;
+    blurWidth: number;
+  };
+  price: number;
+  title: string;
+  type: string;
+};
 
 const Order = () => {
+  const [getUrl, setGetUrl] = useState<queryData>();
+
+  useEffect(() => {
+    // URL에서 쿼리 문자열 가져온 후 item을 JSON 파싱하여 JavaScript 객체로 변환
+    const queryString = window.location.search;
+
+    const urlParams = new URLSearchParams(queryString);
+    const itemDataString = urlParams.get('item');
+
+    const parsedItemData = JSON.parse(itemDataString);
+
+    setGetUrl(parsedItemData);
+  }, []); 
+
+  console.log(getUrl)
+  
   return (
     <div>
-        <Header title="주문결제" type="subMenu"/>
+      <Header title="주문/결제" type="subMenu" />
     </div>
-  )
-}
+  );
+};
 
-export default Order
+export default Order;
