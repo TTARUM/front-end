4d40045878f7e1ad9ab2817f408956dba57093d3
evan <evan@vers.kr>
2024-03-13 17:33:13 +0900

    [TTAR - 23] 주문/결제 페이지 작업 중

--- src/app/order/page.tsx
+++ src/app/order/page.tsx
@@ -8,6 +8,10 @@ import Image from 'next/image';
 
 import downArrow from '../../../public/grayDownArrow.svg';
 import upArrow from '../../../public/grayUpArrow.svg';
+import checked from '../../../public/checked.svg';
+import check from '../../../public/check.svg';
+import check_on from '../../../public/check_on.svg';
+import { MainEventButton } from '@/components/Style/MainEventBtn/MainEventBtn';
 
 type queryData = {
   id: number;
@@ -27,6 +31,13 @@ type queryData = {
 const Order = () => {
   const [getUrl, setGetUrl] = useState<queryData>();
   const [showOrderedItem, setShowOrderedItem] = useState<boolean>(false);
+  const [getPayment, setGetPayment] = useState<string>('card');
+  const [rememberPayment, setRememberPayment] = useState<boolean>(false);
+  const [agreeTreatment, setAgreeTreatment] = useState<boolean>(false);
+  const [agreeCollection, setAgreeCollection] = useState<boolean>(false);
+  const [deliveryRequest, setDeliveryRequest] =
+    useState<string>('배송 요청사항');
+  const [showDeliveryPopup, setShowDeliveryPopup] = useState<boolean>(false);
 
   useEffect(() => {
     // URL에서 쿼리 문자열 가져온 후 item을 JSON 파싱하여 JavaScript 객체로 변환
@@ -48,6 +59,7 @@ const Order = () => {
   return (
     <div className="order_class">
       <Header title="주문/결제" type="subMenu" />
+
       <div className="delivery_select_area">
         <div>
           <button>기본</button>
@@ -56,6 +68,7 @@ const Order = () => {
         <button>배송지 목록</button>
       </div>
       <div className="order_wrap">
+        {/* 배송 */}
         <div className="delivery_address">
           <div>
             <h1>우리집</h1>
@@ -63,12 +76,29 @@ const Order = () => {
           </div>
           <p>홍길동 ∙ 010-0000-0000</p>
           <p>서울 성동구 뚝섬로 273, 1001호 [04770]</p>
+          <div
+            className="requestOption"
+            onClick={() => {
+              setShowDeliveryPopup(true);
+            }}
+          >
+            <p>
+              {deliveryRequest === '기타' ? '배송 요청사항' : deliveryRequest}
+            </p>
+            <Image src={downArrow} alt="downArrow" />
+          </div>
+          <input type='text' placeholder='배송메모를 입력해주세요.' />
+        </div>
+
+        <div className="coupon">
+          <p>쿠폰</p>
           <div className="requestOption">
-            <p>배송 요청사항</p>
+            <p>사용가능 쿠폰 0장 / 전체 n장</p>
             <Image src={downArrow} alt="downArrow" />
           </div>
         </div>
 
+        {/* 주문 상품 */}
         <div className="ordered_product">
           <div className={showOrderedItem == true ? 'title active' : 'title'}>
             <p>주문상품</p>
@@ -98,6 +128,7 @@ const Order = () => {
             : null}
         </div>
 
+        {/* 결제금액 */}
         <div className="total_amount">
           <p>결제금액</p>
           <div>
@@ -106,7 +137,7 @@ const Order = () => {
           </div>
           <div>
             <p>배송비</p>
-            <p>{(totalAmount >= 100000? 0 : 3000).toLocaleString()} 원</p>
+            <p>{(totalAmount >= 100000 ? 0 : 3000).toLocaleString()} 원</p>
           </div>
           <div>
             <p>쿠폰할인</p>
@@ -114,14 +145,143 @@ const Order = () => {
           </div>
           <div>
             <p>총 결제 금액</p>
-            <p>00,000 원</p>
+            <p>
+              {(
+                totalAmount + (totalAmount >= 100000 ? 0 : 3000)
+              ).toLocaleString()}{' '}
+              원
+            </p>
           </div>
         </div>
 
+        {/* 결제 방식 */}
         <div className="payment">
-            <p>결제방식</p>
+          <p>결제방식</p>
+          <div>
+            <div
+              onClick={() => {
+                setGetPayment('card');
+              }}
+              className={
+                getPayment == 'card'
+                  ? 'select_itemBox active'
+                  : 'select_itemBox'
+              }
+            >
+              <Image src={checked} alt="checked" />
+            </div>
+            <p>일반결제</p>
+          </div>
+          <div>
+            <div
+              onClick={() => {
+                setGetPayment('account');
+              }}
+              className={
+                getPayment == 'account'
+                  ? 'select_itemBox active'
+                  : 'select_itemBox'
+              }
+            >
+              <Image src={checked} alt="checked" />
+            </div>
+            <p>계좌 간편결제</p>
+          </div>
+          <div>
+            <Image
+              onClick={() => {
+                setRememberPayment(!rememberPayment);
+              }}
+              src={rememberPayment === true ? check_on : check}
+              alt="check"
+            />
+            <p>이 결제수단을 다음에도 사용하기</p>
+          </div>
+        </div>
+
+        {/* 동의 */}
+        <div className="agreement">
+          <div>
+            <div>
+              <Image
+                onClick={() => {
+                  setAgreeTreatment(!agreeTreatment);
+                }}
+                src={agreeTreatment === true ? check_on : check}
+                alt="check"
+              />
+              <p>개인정보 취급 위탁 동의</p>
+            </div>
+            <p>보기</p>
+          </div>
+          <div>
+            <div>
+              <Image
+                onClick={() => {
+                  setAgreeCollection(!agreeCollection);
+                }}
+                src={agreeCollection === true ? check_on : check}
+                alt="check"
+              />
+              <p>개인정보 수집 및 이용 동의</p>
+            </div>
+            <p>보기</p>
+          </div>
+          <MainEventButton
+            disabled={
+              agreeTreatment == false || agreeCollection == false ? true : false
+            }
+            width={345}
+            height={41}
+            color={
+              agreeTreatment == false || agreeCollection == false
+                ? '#999999'
+                : '#FF6135'
+            }
+          >
+            결제하기
+          </MainEventButton>
         </div>
       </div>
+
+      {showDeliveryPopup === true ? (
+        <div className="delivery_popup">
+          <div>
+            <p
+              onClick={() => {
+                setDeliveryRequest('문 앞에 놔주세요.');
+                setShowDeliveryPopup(false);
+              }}
+            >
+              문 앞에 놔주세요.
+            </p>
+            <p
+              onClick={() => {
+                setDeliveryRequest('택배함에 놔주세요.');
+                setShowDeliveryPopup(false);
+              }}
+            >
+              택배함에 놔주세요.
+            </p>
+            <p
+              onClick={() => {
+                setDeliveryRequest('경비실에 맡겨주세요.');
+                setShowDeliveryPopup(false);
+              }}
+            >
+              경비실에 맡겨주세요.
+            </p>
+            <p
+              onClick={() => {
+                setDeliveryRequest('기타');
+                setShowDeliveryPopup(false);
+              }}
+            >
+              기타
+            </p>
+          </div>
+        </div>
+      ) : null}
     </div>
   );
 };
