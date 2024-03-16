f3029d500976ec2f38cdbdd25c3142cb6fae6f66
JangWonjung <jangexpedition@gmail.com>
2024-03-16 19:17:45 +0900

    [feat] 배송지 추가 페이지 UI 완성, InputText 컴포넌트 추가

--- src/app/order/deliveryAdd/page.tsx
+++ src/app/order/deliveryAdd/page.tsx
@@ -3,13 +3,67 @@ import Header from '@/components/Header/Header';
 import './deliveryAdd.scss';
 
 import { useRouter } from 'next/navigation';
+import useInput from '@/hooks/useInput';
+import InputText from '@/components/InputText/InputText';
+import { useState } from 'react';
 
 const DeliveryAdd = () => {
   const router = useRouter();
 
+  const [alias, setAlias] = useState('');
+  const [receiver, setReceiver] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+
+  const inputDataArr = [
+    {
+      data: alias,
+      setData: setAlias,
+      title: '별칭',
+      placeholder: '별칭을 입력해주세요.',
+    },
+    {
+      data: receiver,
+      setData: setReceiver,
+      title: '받는 사람',
+      placeholder: '받는 사람을 입력해주세요.',
+    },
+    {
+      data: address,
+      setData: setAddress,
+      title: '주소',
+      placeholder: '우편 번호를 입력해주세요.',
+      type: 'search',
+    },
+    {
+      data: phone,
+      setData: setPhone,
+      title: '휴대폰 번호',
+      placeholder: '휴대폰 번호를 입력해주세요.',
+    },
+  ];
+
   return (
     <div className="deliveryAdd_container">
       <Header title="배송지 추가" type="subMenu" />
+      <div className="deliveryAdd_wrapper">
+        {inputDataArr.map((inputData) => (
+          <InputText
+            key={inputData.title}
+            data={inputData.data}
+            setData={inputData.setData}
+            title={inputData.title}
+            placeholder={inputData.placeholder}
+            type={inputData.type}
+          />
+        ))}
+        <div className="checkbox_wrapper">
+          <input type="checkbox" id="setDefault"></input>
+          <label htmlFor="setDefault"></label>
+          <label htmlFor="setDefault">기본 배송지로 선택</label>
+        </div>
+        <button className="save_button">저장</button>
+      </div>
     </div>
   );
 };
